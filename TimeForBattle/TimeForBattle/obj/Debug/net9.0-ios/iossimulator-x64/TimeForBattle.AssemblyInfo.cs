f3b93f56cfb757f62b6da1f45cfe234192ea0e48
Halibenar <martijn.boot@outlook.com>
2026-01-16 14:49:24 +0100

    Added condition info button
    Added condition info button, which opens a popup window with rule information about the condition.

--- TimeForBattle/TimeForBattle/obj/Debug/net9.0-ios/iossimulator-x64/TimeForBattle.AssemblyInfo.cs
+++ TimeForBattle/TimeForBattle/obj/Debug/net9.0-ios/iossimulator-x64/TimeForBattle.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimeForBattle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6d19f0fc25ffec3ce8b67a1a09da957b9241d1c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+05b457bb563a9ccdaa11916c3dab71bb383558e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimeForBattle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimeForBattle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
